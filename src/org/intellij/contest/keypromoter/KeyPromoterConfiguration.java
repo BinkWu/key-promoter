d9c61384551414c8cb4dce450f68e66726e6df3e
dmitry.kashin <dmitry.kashin@53b4dc2c-401d-0410-894d-118481a27811>
2006-09-12 11:57:10 +0000

    Initial checkin.

---
+++ src/org/intellij/contest/keypromoter/KeyPromoterConfiguration.java
@@ -0,0 +1,123 @@
+package org.intellij.contest.keypromoter;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.util.JDOMExternalizable;
+import com.intellij.openapi.util.InvalidDataException;
+import com.intellij.openapi.util.WriteExternalException;
+import com.intellij.openapi.util.DefaultJDOMExternalizer;
+import com.intellij.ui.ColorPanel;
+
+import javax.swing.*;
+
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NonNls;
+import org.jdom.Element;
+
+
+/**
+ * Date: 07.09.2006
+ * Time: 11:19:46
+ */
+public class KeyPromoterConfiguration implements Configurable, ApplicationComponent, JDOMExternalizable  {
+
+    JPanel myConfigPanel;
+    private JSpinner myDisplayTime;
+    private JCheckBox myAllButtons;
+    private JCheckBox myToolWindowButtons;
+    private JCheckBox myToobarButtons;
+    private JCheckBox myMenus;
+    private JSpinner myAnimationDelay;
+    private ColorPanel myTextColor;
+    private ColorPanel myBackgroundColor;
+    private ColorPanel myBorderColor;
+    private JSpinner myFontSize;
+
+    private KeyPromoterSettings mySettings = new KeyPromoterSettings();
+
+    public String getDisplayName() {
+        return "KeyPromoter";
+    }
+
+    public Icon getIcon() {
+        return null;
+    }
+
+    @Nullable
+    @NonNls
+    public String getHelpTopic() {
+        return null;
+    }
+
+    public JComponent createComponent() {
+        return myConfigPanel;
+    }
+
+    public boolean isModified() {
+        if (myMenus.isSelected() != mySettings.isMenusEnabled()) return true;
+        if (myToobarButtons.isSelected() != mySettings.isToolbarButtonsEnabled()) return true;
+        if (myToolWindowButtons.isSelected() != mySettings.isToolWindowButtonsEnabled()) return true;
+        if (myAllButtons.isSelected() != mySettings.isAllButtonsEnabled()) return true;
+        if (myTextColor.getSelectedColor() != mySettings.getTextColor()) return true;
+        if (myBackgroundColor.getSelectedColor() != mySettings.getBackgroundColor()) return true;
+        if (myBorderColor.getSelectedColor() != mySettings.getBorderColor()) return true;
+        if (!myDisplayTime.getValue().equals(mySettings.getDisplayTime())) return true;
+        if (!myAnimationDelay.getValue().equals(mySettings.getFlashAnimationDelay())) return true;
+        if (!myFontSize.getValue().equals(mySettings.getFontSize())) return true;
+        return false;
+    }
+
+    public void apply() throws ConfigurationException {
+        mySettings.setMenusEnabled(myMenus.isSelected());
+        mySettings.setToolbarButtonsEnabled(myToobarButtons.isSelected());
+        mySettings.setToolWindowButtonsEnabled(myToolWindowButtons.isSelected());
+        mySettings.setAllButtonsEnabled(myAllButtons.isSelected());
+        mySettings.setTextColor(myTextColor.getSelectedColor());
+        mySettings.setBackgroundColor(myBackgroundColor.getSelectedColor());
+        mySettings.setBorderColor(myBorderColor.getSelectedColor());
+        mySettings.setDisplayTime(new Integer(myDisplayTime.getValue().toString()));
+        mySettings.setFlashAnimationDelay(new Integer(myAnimationDelay.getValue().toString()));
+        mySettings.setFontSize(new Integer(myFontSize.getValue().toString()));
+    }
+
+    public void reset() {
+        myMenus.setSelected(mySettings.isMenusEnabled());
+        myToobarButtons.setSelected(mySettings.isToolbarButtonsEnabled());
+        myToolWindowButtons.setSelected(mySettings.isToolWindowButtonsEnabled());
+        myAllButtons.setSelected(mySettings.isAllButtonsEnabled());
+        myTextColor.setSelectedColor(mySettings.getTextColor());
+        myBackgroundColor.setSelectedColor(mySettings.getBackgroundColor());
+        myBorderColor.setSelectedColor(mySettings.getBorderColor());
+        myDisplayTime.setValue(mySettings.getDisplayTime());
+        myAnimationDelay.setValue(mySettings.getFlashAnimationDelay());
+        myFontSize.setValue(mySettings.getFontSize());
+    }
+
+    public void disposeUIResources() {
+    }
+
+    @NonNls
+    public String getComponentName() {
+        return "KeyPromoter.Config";
+    }
+
+    public void initComponent() {
+    }
+
+    public void disposeComponent() {
+    }
+
+    public KeyPromoterSettings getSettings() {
+        return mySettings;
+    }
+
+    public void readExternal(Element element) throws InvalidDataException {
+        DefaultJDOMExternalizer.readExternal(mySettings, element);
+    }
+
+    public void writeExternal(Element element) throws WriteExternalException {
+        DefaultJDOMExternalizer.writeExternal(mySettings, element);
+    }
+
+}
---
+++ src/org/intellij/contest/keypromoter/Test.java
@@ -0,0 +1,43 @@
+package org.intellij.contest.keypromoter;
+
+public class Test {
+    private boolean bu;
+    private boolean zu;
+    private boolean mu;
+    private boolean ku;
+
+    public Test() {
+    }
+
+    public boolean isBu() {
+        return bu;
+    }
+
+    public void setBu(final boolean bu) {
+        this.bu = bu;
+    }
+
+    public boolean isZu() {
+        return zu;
+    }
+
+    public void setZu(final boolean zu) {
+        this.zu = zu;
+    }
+
+    public boolean isMu() {
+        return mu;
+    }
+
+    public void setMu(final boolean mu) {
+        this.mu = mu;
+    }
+
+    public boolean isKu() {
+        return ku;
+    }
+
+    public void setKu(final boolean ku) {
+        this.ku = ku;
+    }
+}
